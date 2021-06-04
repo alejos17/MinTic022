a68e47edec0741591af199801c3f64f2a393dc80
alejos17 <alejos17@gmail.com>
2021-06-04 13:05:14 -0500

    reto5-inicio2

--- Ciclo1/Semana5/Reto5/main.py
+++ Ciclo1/Semana5/Reto5/main.py
@@ -4,7 +4,26 @@ TODO
     - documento
     - nombre
     - apellido
+    - etapa del paciente
     - fecha nacimeinto
+    - edad
+    - fecha de cita
+    - tipo de vacuna aplicada
+
+2. Guardar tupla tipo paciente con los datos
+3. Guardar una lista de pacientes con la tupla
+4. contar el total de pacientes en la lista para el primer reporte
+5. sacar los datos por cada tipo de vacuna
+6. armar nuevas tuplas por tipo de vacuna
+7. armar listas por vacuna.
+8. sacar reporte por tipo de vacuna
++
+}+
+++
+
+
+
+
 
 
 
